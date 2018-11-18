fc4b6885046f73414bbf2b6e0a58e0c6f297d863
oaassi <mohamed.oaassi94@gmail.com>
2018-11-19 00:24:03 +0100

    Twig descovering

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,9 +3,10 @@
 namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController{
+class ArticleController extends  AbstractController {
 
     /**
      * @Route("/")
@@ -18,9 +19,15 @@ class ArticleController{
      * @Route("/new/{slug}")
      */
     public function show($slug){
-        return new Response(sprintf(
-            'thanks %s',
-            $slug
-        ));
+        $comments=[
+          'fffffffffffffffffffffffffff',
+          'fffbbgbgf',
+            'vfvvvvvv',
+            'fvvvwmohamed'
+        ];
+        return $this->render('article/show.html.twig',[
+           'title' => ucwords(str_replace('_',' ',$slug)),
+            'comments' => $comments
+        ]);
     }
 }
